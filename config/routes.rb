2c9abad8c11dc7a7b888df2020ef8da60f334816
km121500 <km121500@gmail.com>
2021-10-27 23:21:52 +0000

    [finish]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about' ,as: 'about'
-  resources :users, only: [:show,:index,:edit,:update]
-  resources :books, onlu: [:index,:show,:create,:edit,:update,:destroy] do
+  resources :users, only: [:show,:index,:edit,:update] do
+  resource  :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  resources :books, onl: [:index,:show,:create,:edit,:update,:destroy] do
   resources :book_comments, only: [:create, :destroy]
   resource  :favorites, only: [:create, :destroy]
   end
