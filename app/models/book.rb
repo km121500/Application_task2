ae0b211aa6d7efd61d2f6fcafed0949a7cb0e097
km121500 <km121500@gmail.com>
2021-10-31 21:32:28 +0000

    [finish]評価機能おわり

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,17 @@ class Book < ApplicationRecord
   
   	validates :title, presence: true
   	validates :body, presence: true, length: {maximum: 200}
+  	validates :rate, presence: true
+    validates :rate, numericality: {
+    # only_integer: true,
+    less_than_or_equal_to: 3,
+    greater_than_or_equal_to: 1,
+  }
   	
   	belongs_to :user
   	has_many :favorites, dependent: :destroy
   	has_many :book_comments, dependent: :destroy
+  	
   	
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
